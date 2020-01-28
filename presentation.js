5fc8ba5e4ffc537873c43fde2266e07de5edb2fd
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-01-28 11:52:14 +0100

    etape 4 ok

--- presentation.js
+++ presentation.js
@@ -10,11 +10,21 @@ function start() {
         rl.question('saisissez un numéro : ', function(saisie){
             if(saisie == 1){
                 console.log('>> Liste des clients');
+                listerClients();
                 start();
             }else if(saisie == 99){
                 console.log('Au revoir');
                 rl.close();
             }
         })
+}
+function listerClients(){
+    var request = require('request');
+    request('https://btoulemonde-hotel-app.herokuapp.com/clients/', {json : true}, function(err, res,body){
+        if(err){
+            return console.log('Erreur',err);
+        }
+        console.log('Ok', body);
+    });
 }
 exports.start = start;
